3b3c33b1a564212d3b74ee497ad8518a69574181
Sreevani <sreevanigaddam1010@gmail.com>
2024-09-22 14:58:02 +0530

    remove unused code

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,26 +1,9 @@
 import ReportCards from "./ReportCards";
-import ReportCards from "./ReportCards";
 import { WiHumidity } from "react-icons/wi";
-import { FiSunset,FiSunrise } from "react-icons/fi";
+import { FiSunset, FiSunrise } from "react-icons/fi";
 import { TbUvIndex } from "react-icons/tb";
 import "../styles/WeatherReport.css";
 
-const WeatherReport = ({ humidity, sunset, uvindex, sunrise }) => {
-  return (
-    <div className="report">
-      <div className="layout">
-        <ReportCards icon={<WiHumidity />} name="Humidity" value={humidity} />
-        <ReportCards icon={<FiSunset />} name="Sunset" value={sunset} />
-      </div>
-      <div className="layout">
-        <ReportCards icon={<TbUvIndex />} name="UV Index" value={uvindex} />
-        <ReportCards icon={<FiSunrise />} name="Sunrise" value={sunrise} />
-      </div>
-    </div>
-  );
-};
-
-export default WeatherReport;
 const WeatherReport = ({ humidity, sunset, uvindex, sunrise }) => {
   return (
     <div className="report">
